3082acdbeb6a81001bac08533e5c9b437556873d
Pratham5590 <prathamvs2012@gmail.com>
2026-09-14 16:19:13 +0530

    Fixed bug : Form not clearing after submission

--- public/script.js
+++ public/script.js
@@ -2,10 +2,12 @@ const form = document.querySelector("#register");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+
   const name = document.querySelector("#name").value;
   const regClass = document.querySelector("#class").value;
   const school = document.querySelector("#school").value;
   const email = document.querySelector("#email").value;
+
   const response = await fetch("/public", {
     method: "POST",
     headers: {
@@ -18,17 +20,24 @@ form.addEventListener("submit", async (event) => {
       email
     })
   });
+
   const data = await response.json();
+
   if (data.message === "Registration successful") {
     form.style.display = "none";
+
     const confirm = document.createElement("p");
     confirm.classList.add("confirm");
-    confirm.textContent = "Your registration is under review. Please check your email regularly for updates.";
-    document.querySelector('main').append(confirm);
+    confirm.textContent =
+      "Your registration is under review. Please check your email regularly for updates.";
+
+    form.parentElement.appendChild(confirm);
   } else {
     const confirm = document.createElement("p");
     confirm.classList.add("confirm-wrong");
-    confirm.textContent = "We were unable to process your registration. Please try again.";
-    document.querySelector('main').append(confirm);
+    confirm.textContent =
+      "We were unable to process your registration. Please try again.";
+
+    form.parentElement.appendChild(confirm);
   }
 });
